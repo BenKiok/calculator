a7751b66968290df051e2646d5e03c75c1c2d72c
Ben Kioko <benedictmkioko@gmail.com>
2020-02-16 16:47:29 -0500

    Tweak HTML, CSS; Add event listeners for JS Add zero, decimal, equal key. Increase span size and add :hover and :active properties for user ease. Add event listeners with if statements directing the specific function to be assigned to each span, to allow them to function as buttons.

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+const input = document.querySelector('input');
+const keyPad = Array.from(document.querySelectorAll('span'));
+let firstNum;
+let secondNum;
+let operation;
+
+input.value = ''; // saves input value after refreshing browser
+
+keyPad.forEach(function(key) {
+  if (key.textContent == '+' || key.textContent == '-' || key.textContent == '/' || key.textContent == '*') {
+    key.addEventListener('click', () => { pressOperatorKey(key) }, false);
+  } else if (key.textContent == '=') {
+    key.addEventListener('click', pressEqualKey, false);
+  } else {
+    key.addEventListener('click', () => { pressNumberKey(key) }, false);
+  }
+});
+
 function add(num1, num2) {
-  return num1 + num2;
+  return Number(num1) + Number(num2);
 }
 
 function subtract(num1, num2) {
@@ -14,6 +32,34 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 
-function operator(mathFunct, num1, num2) {
-  return mathFunct(num1, num2);
+function operator(funct, num1, num2) {
+  return funct(num1, num2);
+}
+
+function pressOperatorKey(key) {
+  firstNum = input.value;
+  switch (key.textContent) {
+    case '+':
+      operation = add;
+      break;
+    case '-':
+      operation = subtract;
+      break;
+    case '*':
+      operation = multiply;
+      break;
+    case '/':
+      operation = divide;
+      break;
+    }
+  input.value = '';
+}
+
+function pressEqualKey() {
+  secondNum = input.value;
+  input.value = operator(operation, firstNum, secondNum);
+}
+
+function pressNumberKey(key) {
+  input.value += key.textContent;
 }
