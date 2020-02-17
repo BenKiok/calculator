d33ddd2c466b8c94e74f7cd0db4b3e2df3990d13
Ben Kioko <benedictmkioko@gmail.com>
2020-02-17 16:34:46 -0500

    Add AC, sign toggle, percent keys Add AC key that clears second input when attempting an operation and clears both inputs and operation in all other cases, via a boolean variable that records if an operator key is last pressed. Add sign toggle that makes a positive number negative and vice versa. Add percent key that divides an input by 100 to give a decimal form of a percentage.

--- script.js
+++ script.js
@@ -3,18 +3,27 @@ const keyPad = Array.from(document.querySelectorAll('span'));
 let firstNum;
 let secondNum;
 let operation;
+let operatorKeyIsPressed = false;
 
 input.value = ''; // saves input value after refreshing browser
+
 
 keyPad.forEach(function(key) {
   if (key.textContent == '+' || key.textContent == '-' || key.textContent == '/' || key.textContent == 'x') {
     key.addEventListener('click', () => { pressOperatorKey(key) }, false);
   } else if (key.textContent == '=') {
     key.addEventListener('click', pressEqualKey, false);
+  } else if (key.textContent == 'AC') {
+    key.addEventListener('click', () => { pressAC(key) }, false);
+  } else if (key.textContent == '-/+') {
+    key.addEventListener('click', pressSignToggle, false);
+  } else if (key.textContent == '%') {
+    key.addEventListener('click', pressPercentKey, false);
   } else {
     key.addEventListener('click', () => { pressNumberKey(key) }, false);
   }
 });
+
 
 function add(num1, num2) {
   return Number(num1) + Number(num2);
@@ -45,21 +54,57 @@ function pressOperatorKey(key) {
     case '-':
       operation = subtract;
       break;
-    case '*':
+    case 'x':
       operation = multiply;
       break;
     case '/':
       operation = divide;
       break;
     }
+  operatorKeyIsPressed = true;
   input.value = '';
 }
 
 function pressEqualKey() {
   secondNum = input.value;
-  input.value = operator(operation, firstNum, secondNum);
+  if (typeof firstNum == 'string' && typeof secondNum == 'string') {
+    input.value = operator(operation, firstNum, secondNum);
+  }
+
+  firstNum = secondNum;
+  operatorKeyIsPressed = false;
 }
 
 function pressNumberKey(key) {
   input.value += key.textContent;
+}
+
+function pressAC() {
+  if (operatorKeyIsPressed) {
+    console.log('cleared input');
+    input.value = '';
+    operatorKeyIsPressed = false;
+  } else {
+    console.log('cleared all');
+    firstNum = undefined;
+    secondNum = undefined;
+    operation = undefined;
+    input.value = '';
+  }
+}
+
+function pressSignToggle() {
+  if (input.value[0] == '-') {
+    let inverseInput = '';
+    for(let i = 1; i < input.value.length; i++) {
+      inverseInput += input.value[i];
+    };
+    input.value = inverseInput;
+  } else {
+    input.value = `-${input.value}`;
+  }
+}
+
+function pressPercentKey() {
+  input.value /= 100;
 }
